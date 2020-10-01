4d0e9f4b41153816e03b0859def717a2bd929b55
Patrick Dougherty <patrick.r.dougherty@und.edu>
2020-09-30 19:53:49 -0500

    Finish HW5

--- HW5/hw5-PatrickDoughertyDebug.c
+++ HW5/hw5-PatrickDoughertyDebug.c
@@ -34,47 +34,53 @@ void* threadOne(void* arg)
     char readVal;
     int threadNum;
     struct threadData *data;
-    
     //Access data for thread number while ensuring main doesn't change data
+    printf("A thread started\n");
     pthread_mutex_lock(&mutex);
     data = (struct threadData *) arg;
     threadNum = data->thread;
+    printf("Thread %d started\n", threadNum);
     pthread_cond_signal(&empty);
     pthread_mutex_unlock(&mutex);
+    printf("Thread %d released mutex\n", threadNum);
     pthread_barrier_wait(&barrier);
-    
-    //Generate input filename and open file
+
     char fileName[9];
     sprintf(fileName, "hw5-%d.in", (threadNum + 1));
     FILE *readFile = fopen(fileName, "r");
+    printf("Thread %d opened %s\n", threadNum, fileName);
     
-    //Read characters from input file and copy to shared variable
     while (fscanf(readFile, "%c\n", &readVal) != EOF)
     {
         pthread_mutex_lock(&mutex);
         while (data->count > 0 || data->turn != threadNum)
         {
+            printf("Thread %d waiting\n", threadNum);
             pthread_cond_wait(&full, &mutex);
         }
+        printf("Thread %d done waiting\n", threadNum);
         data->sharedChar = readVal;
         data->count++;
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
-    //Only thread 0 needs to set done flag
     if (threadNum == 0)
     //All files are the same size, therefore if one is done they all are
     {
         pthread_mutex_lock(&mutex);
         while (data->count > 0 || data->turn != threadNum)
         {
+            printf("Thread 0 waiting to signal done\n");
             pthread_cond_wait(&full, &mutex);
         }
+        printf("Thread 0 signaling done\n");
         data->done = 1;
         data->count++;
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
+    printf("Thread %d exiting\n", threadNum);
+
     fclose(readFile);
     pthread_exit(NULL);
 }
@@ -98,7 +104,6 @@ int main(void)
     
     FILE *writeFile = fopen("hw5.out", "w");
 
-    //create threads while waiting for them to copy thread number
     for (int i = 0; i < 3; i++)
     {
         pthread_mutex_lock(&mutex);
@@ -106,40 +111,51 @@ int main(void)
         pthread_create(&thread[i], NULL, threadOne, (void*) &data);
         pthread_cond_wait(&empty, &mutex); //wait until thread signals it read data
         pthread_mutex_unlock(&mutex);
+        printf("Thread %d created\n", i);
     }
+    printf("All threads created, starting main loop\n");
 
-    //Copy characters from shared variable and write them to output file
     while (1)
     {
         pthread_mutex_lock(&mutex);
         while (data.count < 1)
         {
+            printf("Main waiting\n");
             pthread_cond_wait(&empty, &mutex);
         }
+        printf("Main done waiting\n");
         if (data.done && data.turn == 0)
         {
+            printf("Main signaled done\n");
             pthread_mutex_unlock(&mutex);
             break;
         }
         writeVal = data.sharedChar;
         data.count--;
         data.turn = (data.turn + 1) % 3;
+        printf("Main recieved %c, turn now %d\n", writeVal, data.turn);
         pthread_cond_broadcast(&full);
         pthread_mutex_unlock(&mutex);
         fprintf(writeFile, "%c\n", writeVal);
     }
-    //join threads
+    printf("Main loop exited\n");
+  
     for (int i=0; i<3; i++)
     {
         if(pthread_join(thread[i], NULL) != 0)
         {
             printf("Thread %d failed to join\n", i);
         }
+        else
+        {
+            printf("Thread %d joined\n", i);
+        }
     }
+    printf("All threads joined, closing writeFile\n");
 
     fclose(writeFile);
+    printf("File closed\n");
 
-    //ensure pthread barriers, conditions and mutexs are destroyed
     if(pthread_barrier_destroy(&barrier) != 0)
         printf("barrier destroy failed\n");
     if(pthread_cond_destroy(&full) != 0)
@@ -149,7 +165,9 @@ int main(void)
     if(pthread_mutex_destroy(&mutex) != 0)
         printf("mutex destroy failed\n");
     
+    printf("everything should be done\n");
     return 0;
+    printf("This shouldn't happen\n");
 }
 
 
